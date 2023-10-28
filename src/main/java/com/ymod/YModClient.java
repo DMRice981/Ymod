c0645b9ecf23485c09bf74ac9dcc84e84dfa02d6
尹 <1608806452@qq.com>
2023-10-28 13:18:13 +0800

    ymod

---
+++ src/main/java/com/ymod/YModClient.java
@@ -0,0 +1,10 @@
+package com.ymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
